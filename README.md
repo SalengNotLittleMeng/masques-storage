d3128a68f0970462df4cf8ee95e8e48db670f820
闫韬蒙 <3094969938@qq.com>
2023-02-21 23:56:42 +0800

    docs:更新了文档

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 ## 简介
 
-masques 中的内置的浏览器存储模块，可以实现本地存储的统一管理，序列化，过期时间设置，存储加密，命名空间，响应式hook，内置LRU缓存方法支持，可以轻松实现对于缓存的管理
+masques 中的内置的浏览器存储模块，可以实现本地存储的统一管理，序列化，过期时间设置，存储加密，命名空间，响应式 hook，内置 LRU 缓存方法支持，可以轻松实现对于缓存的管理
 
-masques（基于Vue3的中后台解决方案）的整体功能集成模板：：https://github.com/SalengNotLittleMeng/masques
+masques（基于 Vue3 的中后台解决方案）的整体功能集成模板：：https://github.com/SalengNotLittleMeng/masques
 
 项目 GitHub 地址：https://github.com/SalengNotLittleMeng/masques-storage
 
@@ -12,21 +12,29 @@ masques（基于Vue3的中后台解决方案）的整体功能集成模板：：
 
 创建实例：
 
-```js
+useMasquesStorage 是一个工厂对象，接收两个参数 type 和 options，内部会根据 type 的值，返回不同的 storage 对象，做不同的操作
 
-    import  useMasquesStorage from 'masquesStorage';
-    const storage= useMasquesStorage('local',{
-        //设置超时时间
-        timeout:100000,
-        //是否加密
-        isEncryped:true,
-        //命名空间
-        nameSpace:'main'
-    })
-    //以对象形式存储，可以存储多个值
-    storage.set({key:value})
-    console.log(storgae.get('key'))
-    console.log(storage.get('key',{isRow:true})) //获取原始值
+```js
+import useMasquesStorage from 'masquesStorage';
+const storage = useMasquesStorage('local', {
+  //设置超时时间
+  timeout: {
+    time: 100,
+    reset: true, //是否获取时重设时间
+  },
+  //是否加密
+  isEncryped: true,
+  //命名空间
+  nameSpace: 'main',
+});
+//以对象形式存储，可以存储多个值
+storage.set({ key: value });
+console.log(storgae.get('key'));
+console.log(storage.get('key', { isRow: true })); //获取原始值
 ```
 
-使用hook:
+如果第一个参数是'session',则操作的是 sessionStorage，会自动进行序列化操作
+
+如果第一个参数是'encryp',则返回的是加密对象，有两个方法
+
+使用 hook:
--- src/local/setTimeout.js
+++ src/local/setTimeout.js
@@ -1,35 +1,37 @@
 //设置超时功能
-export function setTimeoutForSetter(decorativeObject,options){
-        if(!options.timeout){
-            decorativeObject.timeout=-1  
-        }else{
-            if(typeof options.timeout=='object'){
-                decorativeObject.timeout=new Date().getTime()+(options.timeout.time || 0)
-                decorativeObject.timeoutRow=options.timeout.time || 0
-                decorativeObject.resetTimeout=true
-            }else if(typeof options.timeout=='number'){
-                decorativeObject.timeout=new Date().getTime()+(options.timeout || 0)
-            }else{
-                throw new TypeError('timeout should be a number or object')
-            }
-        }
+export function setTimeoutForSetter(decorativeObject, options) {
+  if (!options.timeout) {
+    decorativeObject.timeout = -1;
+  } else {
+    if (typeof options.timeout == 'object') {
+      decorativeObject.timeout = new Date().getTime() + (options.timeout.time || 0);
+      if (options.timeout.reset) {
+        decorativeObject.timeoutRow = options.timeout.time || 0;
+        decorativeObject.resetTimeout = true;
+      }
+    } else if (typeof options.timeout == 'number') {
+      decorativeObject.timeout = new Date().getTime() + (options.timeout || 0);
+    } else {
+      throw new TypeError('timeout should be a number or object');
+    }
+  }
 }
-export function timeoutForGetter(decorativeObject,key,vm){
-        //检验超时并删除
-        const {value,timeout,resetTimeout=false}=decorativeObject
-        if(checkTimeout(decorativeObject)){
-            vm.storageHandler.delete(key)
-            return null
-        }else{
-            if(resetTimeout){
-                decorativeObject.timeout=new Date().getTime()+decorativeObject['timeoutRow']
-                localStorage.setItem(key,JSON.stringify(decorativeObject))
-            }
-        }
-        return value
+export function timeoutForGetter(decorativeObject, key, vm) {
+  //检验超时并删除
+  const { value, timeout, resetTimeout = false } = decorativeObject;
+  if (checkTimeout(decorativeObject)) {
+    vm.storageHandler.delete(key);
+    return null;
+  } else {
+    if (resetTimeout) {
+      decorativeObject.timeout = new Date().getTime() + decorativeObject['timeoutRow'];
+      localStorage.setItem(key, JSON.stringify(decorativeObject));
+    }
+  }
+  return value;
 }
 //检验是否超时
-function checkTimeout(decorativeObject){
-    const {timeout}=decorativeObject
-    return timeout==-1?false:new Date().getTime()>timeout
+function checkTimeout(decorativeObject) {
+  const { timeout } = decorativeObject;
+  return timeout == -1 ? false : new Date().getTime() > timeout;
 }
