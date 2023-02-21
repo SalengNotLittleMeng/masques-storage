94ad2a97c16fe161d716b7b7ab0b37c46f4f09f4
yantaomeng <3094969938@qq.com>
2023-02-21 10:53:38 +0800

    feat:新增了自定义事件观测

--- src/observe/index.js
+++ src/observe/index.js
@@ -1,14 +1,42 @@
-const methodsList=['getItem','setItem','clear','removeItem']
+//观测localstorage的值，由于存储对象的方法跟Storage以子类的调用关系是强绑定，
+//因此这里采用的方案是修改Storage对象的原型链调用
+const STORAGEEVENT='store'
 export default class Observe{
-    constructor(options){
-        this.reWriteLocalMethods()
+    constructor(options={}){
+        //确认观测的对象
+        this.storageType=this.ensureObserveType(options)
+        this.reWriteStorageMethods()
     }
-    reWriteLocalMethods(){
+    //重写存储的方法
+    reWriteStorageMethods(){
+        const that=this
+        const methodsList=['getItem','setItem','clear','removeItem']
         methodsList.forEach(method=>{
-            const _localStorageFn=localStorage[method]
             Storage.prototype[method] = (function(key, value) {
-                this.call(localStorage, key, value);
+                //创建自定义事件并触发
+                dispatchEvent(that.createCustomEvent({detail
+                :{
+                    type:method,
+                    key,
+                    value
+                }
+                }))
+                //将this跟存储对象绑定调用
+                this.call(that.storageType, key, value);
             }).bind(Storage.prototype[method]);
         })
+    }
+    //创建可以传递信息的自定义事件对象
+    createCustomEvent(options={}){
+        return new CustomEvent(STORAGEEVENT,options)
+    }
+    //确认观测的对象
+    ensureObserveType(options){
+        const type=options.type || 'local'
+        if(type=='session'){
+            return sessionStorage
+        }else {
+            return localStorage
+        }
     }
 }
